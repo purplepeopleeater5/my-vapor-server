26190075650a9266f7f44925506b234e63e8037c
Tanner <tannerbennett@Mac.lan>
2025-04-18 11:49:09 -0700

    Describe your changes

--- Package.swift
+++ Package.swift
@@ -8,19 +8,20 @@ let package = Package(
     ],
     dependencies: [
         // Vapor framework
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
+        .package(url: "https://github.com/vapor/vapor.git",                   from: "4.110.1"),
         // ORM core
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        .package(url: "https://github.com/vapor/fluent.git",                  from: "4.9.0"),
         // Postgres driver
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git",  from: "2.8.0"),
         // JWT integration for Vapor
-        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/jwt.git",                     from: "4.0.0"),
         // SwiftNIO core
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/apple/swift-nio.git",               from: "2.65.0"),
 
         // Queues for background jobs (delta sync)
-        .package(url: "https://github.com/vapor/queues.git",               from: "1.8.0"),
-        .package(url: "https://github.com/vapor/queues-fluent-driver.git", from: "1.5.0"),
+        .package(url: "https://github.com/vapor/queues.git",                  from: "1.8.0"),
+        // Use SSH URL to avoid HTTPS auth issues
+        .package(url: "git@github.com:vapor/queues-fluent-driver.git",        from: "1.5.0"),
     ],
     targets: [
         .executableTarget(
