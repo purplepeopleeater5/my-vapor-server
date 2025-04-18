12f102d6245edd2dd2631bf605d6dcb55d06b85e
Tanner <tannerbennett@Mac.lan>
2025-04-17 18:16:26 -0700

    Describe your changes

--- Sources/MyServer/Migrations/CreateRecipe.swift
+++ Sources/MyServer/Migrations/CreateRecipe.swift
@@ -3,32 +3,30 @@ import Fluent
 struct CreateRecipe: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
         database.schema(Recipe.schema)
-            .id()    // UUID primary key
+            .id()
 
-            //––– Scalar fields (all nullable) –––
-            .field("title",            .string)    // no .required → NULLABLE
-            .field("description",      .string)
-            .field("cookTime",         .string)
-            .field("prepTime",         .string)
-            .field("servings",         .string)
-            .field("imageURL",         .string)
-            .field("domainURL",        .string)
-            .field("nutritionalInfo",  .string)
-            .field("rating",           .double)
-            .field("ratingCount",      .double)
-            .field("note",             .string)
+            .field("id1",            .string, .required)     // ← add this to back your @Field(key: "id1")
+            .field("title1",         .string)               // ← was "title"
+            .field("description1",   .string)               // ← was "description"
+            .field("cookTime1",      .string)               // ← was "cookTime"
+            .field("prepTime1",      .string)
+            .field("servings1",      .string)
+            .field("imageURL1",      .string)
+            .field("domainURL1",     .string)
+            .field("nutritionalInfo1", .string)
+            .field("rating1",        .double)
+            .field("ratingCount1",   .double)
+            .field("note1",          .string)
             .field("isMealPlanInstance", .bool)
             .field("isNoteOrSection",    .bool)
             .field("isPinned",           .bool)
-            .field("pinnedCount",        .int)
+            .field("isPinnedCount",      .int)
             .field("dateAdded",          .datetime)
 
-            //––– Array fields for your “relationships” (all nullable) –––
             .field("ingredients", .array(of: .string))
             .field("methods",     .array(of: .string))
             .field("categories",  .array(of: .string))
             .field("cuisines",    .array(of: .string))
-
             .create()
     }
 
